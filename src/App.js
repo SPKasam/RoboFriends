46a07d990e4d2e49eda4ee47696036b5195b919c
SPKasam <pranithkasam7@gmail.com>
2022-03-02 11:57:01 -0600

    complete app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 
 class App extends Component{
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
   onSearchChange = (event)=>{
     this.setState({searchfield: event.target.value})
+
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users=>{
+        this.setState({robots: users});
+      })
 
   }
 
@@ -27,7 +38,9 @@ class App extends Component{
       <div className = 'tc'>
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots = {filteredRobots}/>
+        <Scroll>
+          <CardList robots = {filteredRobots}/>
+        </Scroll>
       </div>
     )
   }
